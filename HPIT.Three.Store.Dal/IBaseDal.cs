935d16caecb27e01981cbb602b9c9e4d4555c7b5
YY <3111567751@qq.com>
2019-10-31 15:40:02 +0800

    添加项目文件。

---
+++ HPIT.Three.Store.Dal/IBaseDal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace HPIT.Three.Store.Dal
+{
+    public interface IBaseDal<T> where T : class, new()
+    {
+        List<T> Sel(string proc,string name = "",string pwd = "",int par1=0,int par2 = 0);
+
+        bool ins(string proc, T Model);
+
+        bool Del(string proc, T Model);
+
+        bool Upd(string proc, T Model);
+
+    }
+}
+
---
+++ HPIT.Three.Store.Dal/SQLHelper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+namespace HPIT.Three.Store.Dal
+{
+    public class SQLHelper
+    {
+        private static string sql = ConfigurationManager.ConnectionStrings["BookStoreConnectionString"].ConnectionString;
+
+        public static DataTable Sel(string proc, SqlParameter[] par = null)
+        {
+            DataTable table = new DataTable();
+            SqlConnection conn = new SqlConnection(sql);
+            SqlCommand comm = new SqlCommand(proc, conn);
+            comm.CommandType = CommandType.StoredProcedure;
+            if (par != null)
+                comm.Parameters.AddRange(par);
+
+            SqlDataAdapter sda = new SqlDataAdapter(comm);
+            sda.Fill(table);
+            return table;
+        }
+
+        public static bool NanQuery(string proc, SqlParameter[] par = null)
+        {
+            int num = 0;
+            SqlConnection conn = new SqlConnection(sql);
+            SqlCommand comm = new SqlCommand(proc, conn);
+            comm.CommandType = CommandType.StoredProcedure;
+            if (par != null)
+                comm.Parameters.AddRange(par);
+            try
+            {
+                conn.Open();
+                num = comm.ExecuteNonQuery();
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open) 
+                {
+                    conn.Close();
+                }
+            }
+            return num > 0 ? true : false;
+        }
+    }
+}
